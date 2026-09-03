0aeaaa92446ce011b2e5db671a0d350e66807f36
GrntKim <grantjun0224@gmail.com>
2026-09-03 09:27:08 +0900

    chore: add myoj.js header comment and fix index.html title/heading

---
+++ src/script/myoj.js
@@ -0,0 +1,12 @@
+/*
+ * myoj - Make Your Own jQuery
+ *
+ * A minimal jQuery 1.0 reimplementation, built lesson by lesson
+ * to study how the original selector engine, DOM manipulation,
+ * and event system work under the hood.
+ *
+ * Reference: jQuery 1.0 by John Resig (jquery.com), MIT licensed.
+ * See /LICENSE and /reference/jquery-1.0.js.
+ *
+ * Author: GrntKim
+ */
